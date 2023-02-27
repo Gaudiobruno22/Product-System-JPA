5ca3b6138e05f453766d4662adfd1641e3be446f
Bruno Gaudio de Azevedo Mattos <brunogmattos22@gmail.com>
2023-02-27 13:46:52 -0300

    Creating Project

---
+++ src/main/java/com/system/application/SystemStoreApplication.java
@@ -0,0 +1,13 @@
+package com.system.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemStoreApplication.class, args);
+	}
+
+}
