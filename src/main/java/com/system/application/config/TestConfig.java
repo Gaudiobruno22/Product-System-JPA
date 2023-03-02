0f69aea61ef7234d3bf2ae1fdbe42f386b1adf8a
Bruno Gaudio de Azevedo Mattos <brunogmattos22@gmail.com>
2023-03-02 14:07:08 -0300

    Creating Payment Class and your respective OneToOne Assosiation with the Order Entitie

--- src/main/java/com/system/application/config/TestConfig.java
+++ src/main/java/com/system/application/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.system.application.entities.Category;
 import com.system.application.entities.Order;
 import com.system.application.entities.OrderItem;
+import com.system.application.entities.Payment;
 import com.system.application.entities.Product;
 import com.system.application.entities.User;
 import com.system.application.entities.enums.OrderStatus;
@@ -80,6 +81,12 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		//For Dependent Classes, We Don's Call your respective Service, you make a Save from the Independent Class.
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 }
