3808f665cadc8a2d92f25030964392356ab04c2b
Bruno Gaudio de Azevedo Mattos <brunogmattos22@gmail.com>
2023-03-06 17:23:12 -0300

    Handling Product Exceptions

--- src/main/java/com/system/application/services/ProductService.java
+++ src/main/java/com/system/application/services/ProductService.java
@@ -3,11 +3,16 @@ package com.system.application.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.system.application.entities.Product;
 import com.system.application.repositories.ProductRepository;
+import com.system.application.services.exception.DataBaseException;
 import com.system.application.services.exception.ResourceNotFoundException;
 
 @Service
@@ -30,13 +35,32 @@ public class ProductService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
+		catch(RuntimeException e) {
+			e.getMessage();
+		}
 	}
 	
 	public Product update(Long id, Product obj) {
-		Product entity = repository.getReferenceById(id);
-		updateProduct(entity, obj);
-		return repository.save(entity);
+		try {
+			Product entity = repository.getReferenceById(id);
+			updateProduct(entity, obj);
+			return repository.save(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(RuntimeException e) {
+			throw new RuntimeException();
+		}
 	}
 
 	private void updateProduct(Product entity, Product obj) {
