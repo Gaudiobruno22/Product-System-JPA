c2c6375b812ddb3455aea86189041928c07010e2
Bruno Gaudio de Azevedo Mattos <brunogmattos22@gmail.com>
2023-03-07 12:11:55 -0300

    Implementation of Category Services and your respective Controllers and Exceptions

--- src/main/java/com/system/application/services/CategoryService.java
+++ src/main/java/com/system/application/services/CategoryService.java
@@ -3,11 +3,17 @@ package com.system.application.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.system.application.entities.Category;
 import com.system.application.repositories.CategoryRepository;
+import com.system.application.services.exception.DataBaseException;
+import com.system.application.services.exception.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -20,7 +26,57 @@ public class CategoryService {
 	}
 	
 	public Category findById(Long id) {
-		Optional<Category> obj = repository.findById(id);
-		return obj.get();
+		try {
+			Optional<Category> obj = repository.findById(id);
+			return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+		}
+		catch(RuntimeException e) {
+			throw new RuntimeException();
+		}
+	}
+	
+	public Category insert(Category cat) {
+		try {
+			return repository.save(cat);
+		}
+		catch(RuntimeException e) {
+			 e.printStackTrace();
+		}
+		finally {
+			return repository.save(cat);
+		}
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
+		catch(RuntimeException e) {
+			e.getMessage();
+		}
+	}
+	
+	public Category update(Long id, Category obj) {
+		try {
+			Category entity = repository.getReferenceById(id);
+			updateCategory(entity, obj);
+			return repository.save(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(RuntimeException e) {
+			throw new RuntimeException();
+		}
+	}
+
+	private void updateCategory(Category entity, Category obj) {
+		entity.setName(obj.getName());
 	}
 }
